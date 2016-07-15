70f63fbc0c1170bdf4c2557689062b15ce270af7
Leo Ahnn <leoahnn@gmail.com>
2016-07-15 13:49:06 -0400

    finished optimizing sorts

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,15 +1,12 @@
 # insertion_sort.rb
 require 'pry'
-# 1,3,2,5
-#1,2,3,6 =<5
-# 1,3,2,5,7,4,10]
 
 def insert_sort(nums)
   i = 1
   while i < nums.length
     j = i - 1
     temp_var = nums[i]
-    while (j >= 0 && nums[i] < nums[j])
+    while (j >= 0 && temp_var < nums[j])
       nums[j + 1] = nums[j]
       j -= 1
     end
@@ -19,9 +16,7 @@ def insert_sort(nums)
   nums
 end
 
-
-
-
+# create new array version
 def insertion_sort(nums)
   arr = [nums[0]]
   nums[1..-1].each do |num|
@@ -38,34 +33,14 @@ def insertion_sort(nums)
   arr
 end
 
-
-# def insert_sort(nums)
-#   count = 0
-#   nums[count + 1..-1].each do |num|
-#     nums[0..count].each_with_index do |ref, arr_index|
-#       if num < ref
-#         insert(nums, num, arr_index)
-#         count += 1
-#         break
-#       elsif arr_index == count
-#         insert(nums, num, arr_index)
-#         count += 1
-#         break
-#       end
-#     end
-#   end
-#   nums
-# end
-
-
-# def insert(arr, val, index)
-#   for num in (arr.length - 1).downto(index)
-#     arr[num+1] = arr[num]
-#   end
-#   arr[index] = val
-# end
+def insert(arr, val, index)
+  for num in (arr.length - 1).downto(index)
+    arr[num+1] = arr[num]
+  end
+  arr[index] = val
+end
 
 
 
-# p insert_sort([1,3,2,5,7,4,10])
+p insert_sort([5,1,7,4,10])
 p insert_sort([1,4,7,1,3,4,5,-12,0,12,14,5,8,5,1,2,3,-9])
