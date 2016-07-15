820c02566c1931d500aada73ce0feb51ea719eb8
Alex Lach <alexglach@gmail.com>
2016-07-15 13:40:00 -0400

    almost done with optimizing insertion

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -2,6 +2,25 @@
 require 'pry'
 # 1,3,2,5
 #1,2,3,6 =<5
+# 1,3,2,5,7,4,10]
+
+def insert_sort(nums)
+  i = 1
+  while i < nums.length
+    j = i - 1
+    temp_var = nums[i]
+    while (j >= 0 && nums[i] < nums[j])
+      nums[j + 1] = nums[j]
+      j -= 1
+    end
+    nums[j + 1] = temp_var
+    i += 1
+  end
+  nums
+end
+
+
+
 
 def insertion_sort(nums)
   arr = [nums[0]]
@@ -20,33 +39,33 @@ def insertion_sort(nums)
 end
 
 
-def insert_sort(nums)
-  count = 0
-  nums[count + 1..-1].each do |num|
-    nums[0..count].each_with_index do |ref, arr_index|
-      if num < ref
-        insert(nums, num, arr_index)
-        count += 1
-        break
-      elsif arr_index == count
-        insert(nums, num, arr_index)
-        count += 1
-        break
-      end
-    end
-  end
-  nums
-end
+# def insert_sort(nums)
+#   count = 0
+#   nums[count + 1..-1].each do |num|
+#     nums[0..count].each_with_index do |ref, arr_index|
+#       if num < ref
+#         insert(nums, num, arr_index)
+#         count += 1
+#         break
+#       elsif arr_index == count
+#         insert(nums, num, arr_index)
+#         count += 1
+#         break
+#       end
+#     end
+#   end
+#   nums
+# end
 
 
-def insert(arr, val, index)
-  for num in (arr.length - 1).downto(index)
-    arr[num+1] = arr[num]
-  end
-  arr[index] = val
-end
+# def insert(arr, val, index)
+#   for num in (arr.length - 1).downto(index)
+#     arr[num+1] = arr[num]
+#   end
+#   arr[index] = val
+# end
 
 
 
-p insert_sort([1,3,2,5,7,4,10])
+# p insert_sort([1,3,2,5,7,4,10])
 p insert_sort([1,4,7,1,3,4,5,-12,0,12,14,5,8,5,1,2,3,-9])
